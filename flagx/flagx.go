8bf11705195ac5ef3ef599b50ced468eb240f8ab
Trang <40815729+trangmaiq@users.noreply.github.com>
2019-07-18 16:35:09 +0700

    Adding flagx.MustGetStringSlice function (#4)

--- flagx/flagx.go
+++ flagx/flagx.go
@@ -1,32 +1,42 @@
 package flagx
 
 import (
-  "github.com/spf13/cobra"
-  "github.com/trangmaiq/x/cmdx"
+	"github.com/spf13/cobra"
+	"github.com/trangmaiq/x/cmdx"
 )
 
 // MustGetBool returns a bool flag or call cmdx.Fatalf if an error occurs
 func MustGetBool(cmd *cobra.Command, name string) bool {
 	val, err := cmd.Flags().GetBool(name)
 	if err != nil {
-	  cmdx.Fatalf(err.Error())
+		cmdx.Fatalf(err.Error())
 	}
 	return val
 }
 
 // MustGetString returns a string flag or call cmdx.Fatalf if an error occurs
 func MustGetString(cmd *cobra.Command, name string) string {
-  s, err := cmd.Flags().GetString(name)
-  if err != nil {
-    cmdx.Fatalf(err.Error())
-  }
-  return s
+	s, err := cmd.Flags().GetString(name)
+	if err != nil {
+		cmdx.Fatalf(err.Error())
+	}
+	return s
+}
+
+// MustGetStringSlice returns a []string flag or call cmdx.Fatalf id an error occurs
+func MustGetStringSlice(cmd *cobra.Command, name string) []string {
+	ss, err := cmd.Flags().GetStringSlice(name)
+	if err != nil {
+		cmdx.Fatalf(err.Error())
+	}
+
+	return ss
 }
 
 func MustGetInt(cmd *cobra.Command, name string) int {
-  i, err := cmd.Flags().GetInt(name)
-  if err != nil {
-    cmdx.Fatalf(err.Error())
-  }
-  return i
+	i, err := cmd.Flags().GetInt(name)
+	if err != nil {
+		cmdx.Fatalf(err.Error())
+	}
+	return i
 }
