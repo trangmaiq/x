5f58efdc6a5a00812cfc40fe744e45daa5c30330
Trang <40815729+trangmaiq@users.noreply.github.com>
2019-07-18 10:01:21 +0700

    Adding cmdx.Must function (#3)

--- cmdx/helper.go
+++ cmdx/helper.go
@@ -1,16 +1,26 @@
 package cmdx
 
 import (
-  "fmt"
-  "os"
+	"fmt"
+	"os"
 )
+
+// Must fatals with the optional message if err is not nil
+func Must(err error, message string, args ...interface{}) {
+	if err == nil {
+		return
+	}
+
+	_, _ = fmt.Fprintf(os.Stderr, message+"\n", args)
+	os.Exit(1)
+}
 
 // Fatalf write the message to os.Stderr and exit with code 1
 func Fatalf(message string, args ...interface{}) {
 	if len(args) > 0 {
-	  _, _ = fmt.Fprintf(os.Stderr, message+"\n", args...)
+		_, _ = fmt.Fprintf(os.Stderr, message+"\n", args...)
 	} else {
-	  _, _ = fmt.Fprintf(os.Stderr, message)
+		_, _ = fmt.Fprintf(os.Stderr, message)
 	}
 	os.Exit(1)
 }
